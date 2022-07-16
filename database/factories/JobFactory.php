907f726a88a72bf9544c666c07ca25c6f6742c1e
Halim Hmairi <hmairihalim@gmail.com>
2022-07-16 12:26:25 +0100

    job seed , factory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
@@ -17,7 +18,22 @@ class JobFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->name,
+            'jobsummary' => $this->faker->paragraph(1),
+            'description' => $this->faker->paragraph(50),
+            'tags' => $this->faker->randomElement([
+                'php,java,js',
+                'html,css',
+                'data,docker',
+                'springboot,api,ios',
+                'nodejs,api',
+                'react,c,angular',
+            ]),
+            'status' => $this->faker->randomElement([
+                'draft',
+                'published',
+            ]),
+            'publish_at' => now(),
         ];
     }
 }
--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Job;
 class JobSeeder extends Seeder
 {
     /**
@@ -14,6 +14,6 @@ class JobSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Job::factory(10)->create();
     }
 }
